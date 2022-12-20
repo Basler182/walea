6d4646a753a6cb57fdb8dc235d8935c3b3bf48b2
Basler182 <showdialog@web.de>
2022-12-20 21:54:13 +0100

    Initial commit

---
+++ src/main/java/info/walea/api/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package info.walea.api.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
